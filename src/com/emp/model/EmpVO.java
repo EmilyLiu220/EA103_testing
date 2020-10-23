1606b530336024a8e2ebdc86c9d5f7c3ededb975
Emily Liu <iaas0111@gmail.com>
2020-10-23 11:16:07 +0800

    edit EmpVO

--- src/com/emp/model/EmpVO.java
+++ src/com/emp/model/EmpVO.java
@@ -6,7 +6,14 @@ public class EmpVO implements Serializable{
 
 	private static final long serialVersionUID = -1787739322168367097L;
 	private String empNo;
+	private String empName;
 	
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
 	public String getEmpNo() {
 		return empNo;
 	}
